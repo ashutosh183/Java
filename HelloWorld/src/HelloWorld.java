ec693b95d3a5ea2bd0e904f9fea16ffffef485b2
Ashutosh <ashutoshkumarpandey183@gmail.com>
2021-12-04 21:43:46 +0530

    Created HelloWorld

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("hello, world!");
+        System.out.println("Hi Ashutosh");
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+    }
+}
