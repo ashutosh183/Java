9e0e94d439877527bd690a4980a2073940ab6e06
Ashutosh <ashutoshkumarpandey183@gmail.com>
2021-12-09 11:31:50 +0530

    Used charAt()

--- StringBuilders.java
+++ StringBuilders.java
@@ -3,5 +3,8 @@ class StringBuilders{
       public static void main(String[] args){
             StringBuilder sb = new StringBuilder("Ashutosh");
             System.out.println(sb);
+
+            //PRINTING CHAR AT INDEX I
+            System.out.println(sb.charAt(5));
       }
 }
