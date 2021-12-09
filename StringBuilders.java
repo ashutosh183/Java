b45e72286c76516a340bd96e6f5ea8247f7e8a4f
Ashutosh <ashutoshkumarpandey183@gmail.com>
2021-12-09 11:24:52 +0530

    StringBuilder

---
+++ StringBuilders.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+class StringBuilders{
+      public static void main(String[] args){
+            StringBuilder sb = new StringBuilder("Ashutosh");
+            System.out.println(sb);
+      }
+}
